75ba4eee8ac1fa27ede830c8ab7cda4eaf869fb7
wangwei <510271184@qq.com>
2022-08-09 13:57:32 +0800

    完成向Eureka注册中心注册服务

--- sprigncloud-provider-dept-8001/src/main/java/com/kuang/springcloud/controller/DeptController.java
+++ sprigncloud-provider-dept-8001/src/main/java/com/kuang/springcloud/controller/DeptController.java
@@ -4,6 +4,8 @@ import com.kuang.springcloud.pojo.Dept;
 import com.kuang.springcloud.service.DeptService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,11 @@ public class DeptController {
 
   @Autowired
   private DeptService service;
+
+  //获取一些配置的信息,得到具体的微服务
+  @Autowired
+  private DiscoveryClient client;
+
 
 
   @PostMapping("/dept/addDept")
@@ -32,7 +39,24 @@ public class DeptController {
   @GetMapping("/dept/getList")
   public List<Dept> getList() {
     return service.getList();
+  }
 
+  //注册进来的微服务,获取一些消息
+  @GetMapping("/dept/discovery")
+  public Object discovery(){
+    //获取微服务列表的清单
+    List<String> services = client.getServices();
+    System.err.println("discovery->services:"+services);
+    //得到一个具体的微服务信息
+    List<ServiceInstance> instances = client.getInstances("springcloud-provider-dept");
+    instances.forEach(i ->{
+      System.out.println(i.getHost());
+      System.out.println(i.getPort());
+      System.out.println(i.getUri());
+      System.out.println(i.getServiceId());
+        }
+    );
+    return this.client;
   }
 
 
