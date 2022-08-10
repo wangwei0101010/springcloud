b2d29737336940dadf8c8078654291d0be4f4363
wangwei <510271184@qq.com>
2022-08-10 17:30:18 +0800

    Eureka集群配置

---
+++ springcloud-eureka-7003/src/main/java/com/kuang/springcloud/EurekaServer_7003.java
@@ -0,0 +1,16 @@
+package com.kuang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//开启Eureka 服务端的启动类,可以接收别人注册进来
+@EnableEurekaServer
+public class EurekaServer_7003 {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaServer_7003.class,args);
+  }
+
+}
