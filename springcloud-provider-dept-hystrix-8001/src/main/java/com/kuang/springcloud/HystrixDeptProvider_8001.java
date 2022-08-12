918b492bf538319efc09177d4daf556b2d5ebbb1
wangwei <510271184@qq.com>
2022-08-12 16:32:29 +0800

    hystrix集成dashboard监控页面进行监控接口访问情况,具体见dashboard的启动类说明

--- springcloud-provider-dept-hystrix-8001/src/main/java/com/kuang/springcloud/HystrixDeptProvider_8001.java
+++ springcloud-provider-dept-hystrix-8001/src/main/java/com/kuang/springcloud/HystrixDeptProvider_8001.java
@@ -1,12 +1,15 @@
 package com.kuang.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 //启动类
@@ -24,5 +27,19 @@ public class HystrixDeptProvider_8001 {
 
   public static void main(String[] args) {
     SpringApplication.run(HystrixDeptProvider_8001.class,args);
+  }
+
+/**
+ * 监控页面 ,需要开启7001 和hystrix8001,然后访问8001,然后通过url监控页面观察
+ * http://localhost:9001/hystrix/monitor?stream=http%3A%2F%2Flocalhost%3A8001%2Factuator%2Fhystrix.stream&title=demo
+ * */
+  //增加一个servlet,监控页面使用
+  @Bean
+  public ServletRegistrationBean getServlet() {
+    ServletRegistrationBean registrationBean = new ServletRegistrationBean(
+        new HystrixMetricsStreamServlet());
+    registrationBean.addUrlMappings("/actuator/hystrix.stream");
+    return registrationBean;
+
   }
 }
